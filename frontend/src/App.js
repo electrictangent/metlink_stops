cbbc2e3899d31a2c7ed049d37b0e7ff04675c20f
electrictangent <electrictangent@protonmail.com>
2023-03-05 17:59:43 +1300

    More CSS theming work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,115 +1,155 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from "react";
 
 // Font Awesome icon imports
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWheelchair, faClock, faStopwatch, faCalendar, faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWheelchair,
+  faClock,
+  faStopwatch,
+  faCalendar,
+  faMagnifyingGlass,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
-const wheelchairIcon = <FontAwesomeIcon icon={faWheelchair} />
-const stopwatchIcon = <FontAwesomeIcon icon={faStopwatch} />
-const clockIcon = <FontAwesomeIcon icon={faClock} />
-const calendarIcon = <FontAwesomeIcon icon={faCalendar} />
-const magnifyingGlass = <FontAwesomeIcon icon={faMagnifyingGlass} />
-const xMark = <FontAwesomeIcon icon={faXmark} />
+const wheelchairIcon = <FontAwesomeIcon icon={faWheelchair} />;
+const stopwatchIcon = <FontAwesomeIcon icon={faStopwatch} />;
+const clockIcon = <FontAwesomeIcon icon={faClock} />;
+const calendarIcon = <FontAwesomeIcon icon={faCalendar} />;
+const magnifyingGlass = <FontAwesomeIcon icon={faMagnifyingGlass} size="2x" />;
+const xMark = <FontAwesomeIcon icon={faXmark} />;
 
 function statusToIcon(status) {
-  if(status === "On Time"){
+  if (status === "On Time") {
     return " ";
-  } else if( status==="Delayed" ){
-    return clockIcon
-  } else if( status==="Scheduled" ){
-    return calendarIcon
-  } else if( status==="Cancelled"){
-    return xMark
+  } else if (status === "Delayed") {
+    return clockIcon;
+  } else if (status === "Scheduled") {
+    return calendarIcon;
+  } else if (status === "Cancelled") {
+    return xMark;
   }
 }
 
 function getTimeToDepart(arrival, departure) {
-  // TODO: Add code to represent arrival time in mintues from now
-  // below is only a sketch - there are many more variables involved in estimated departure times
-  //   "arrival": {
-  //     "expected": null
-  // },
-  // "departure": {
-  //     "aimed": "2023-03-04T15:14:00+13:00",
-  //     "expected": null
-  // },
   const dateTimeNow = new Date();
 
   // If scheduled trip only, return aimed time for departure
-  if(arrival.expected === null && departure.expected === null){
+  if (arrival.expected === null && departure.expected === null) {
     const dateTime = new Date(departure.aimed);
 
-    return String(dateTime.getHours()) + ":" + String(dateTime.getMinutes()).padStart(2, '0');
-  } else if(arrival.expected === null && departure.expected !== null){
+    return (
+      String(dateTime.getHours()) +
+      ":" +
+      String(dateTime.getMinutes()).padStart(2, "0")
+    );
+  } else if (arrival.expected === null && departure.expected !== null) {
     // If scheduled trip is about to depart
     const dateTime = new Date(departure.expected);
-    return String(Math.round((dateTime - dateTimeNow)*0.001/60)) + " mins";
+    return (
+      String(Math.round(((dateTime - dateTimeNow) * 0.001) / 60)) + " mins"
+    );
   }
 
   // If trip is about to reach stop
   if (arrival.expected !== null) {
-    const dateTime = new Date(arrival.expected)
+    const dateTime = new Date(arrival.expected);
     if (dateTimeNow < dateTime) {
       const dateTimeNow = new Date();
-      return String(Math.round((dateTime - dateTimeNow) * 0.001 / 60)) + " mins";
+      const duemins = Math.round(((dateTime - dateTimeNow) * 0.001) / 60);
+      if (duemins > 0) {
+        return String(duemins) + " mins";
+      } else {
+        return "Delayed";
+      }
     }
   }
 
   // Else, trip is at stop
-  return "Due"
+  return "Due";
 }
 
 function DepartureRow({ singleDeparture }) {
-
-
   // Set the accesible icon
   let accesible = "";
-  if(singleDeparture.wheelchair) {
+  if (singleDeparture.wheelchair) {
     accesible = wheelchairIcon;
   }
 
   return (
-    <tr className={singleDeparture.status === "Cancelled" ? 'cancelled' : ''}>
-      <td className='bold'>{ getTimeToDepart(singleDeparture.arrival, singleDeparture.departure) }</td>
-      <td> 
+    <tr className={singleDeparture.status === "Cancelled" ? "cancelled" : ""}>
+      <td className="bold">
+        {getTimeToDepart(singleDeparture.arrival, singleDeparture.departure)}
+      </td>
+      <td>
         {/* If cancelled, dont color route */}
-        <div className='bold service-num' style={singleDeparture.status === "Cancelled" ? { background : "#686868", color : "#444444" } : {background : singleDeparture.backcolor, color : singleDeparture.forecolor}} >{ singleDeparture.serviceID }</div>
-      </td>  
-      <td> { singleDeparture.direction } </td>
-      <td> <span className={singleDeparture.status === "Delayed" ? 'delayed' : ''}>{ statusToIcon(singleDeparture.status) }</span> { accesible }</td>
+        <div
+          className="bold service-num"
+          style={
+            singleDeparture.status === "Cancelled"
+              ? { background: "#686868", color: "#444444" }
+              : {
+                  background: singleDeparture.backcolor,
+                  color: singleDeparture.forecolor,
+                }
+          }
+        >
+          {singleDeparture.serviceID}
+        </div>
+      </td>
+      <td> {singleDeparture.direction} </td>
+      <td>
+        {" "}
+        <span className="icons">
+          <span
+            className={singleDeparture.status === "Delayed" ? "delayed" : ""}
+          >
+            {statusToIcon(singleDeparture.status)}
+          </span>
+          {accesible}
+        </span>{" "}
+      </td>
     </tr>
   );
 }
 
 function DepartureTable({ departures }) {
   const rows = [];
+  let i = 0;
 
-  departures.forEach((singleDeparture) => {
-    // Ignore first entry as this entry contains stop name and no relevant trip data
-    if(!("stopName" in singleDeparture)){
-      rows.push(<DepartureRow singleDeparture={singleDeparture} key={singleDeparture.tripID} />);
-    }
-  })
+  try {
+    departures.forEach((singleDeparture) => {
+      // Ignore first entry as this entry contains stop name and no relevant trip data
+      if (!("stopName" in singleDeparture)) {
+        rows.push(
+          <DepartureRow
+            singleDeparture={singleDeparture}
+            key={"ind" + String(i) + "_" + singleDeparture.tripID} // Sometimes the API will give duplicate tripIDs, the incrementor ensures all keys are unique.
+          />
+        );
+        i += 1;
+      }
+    });
 
-  return(
-    <table className='table'>
-      <thead>
-        <tr>
-          <th>{stopwatchIcon}</th>
-          <th>ID</th>
-          <th></th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  );
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>{stopwatchIcon}</th>
+            <th>ID</th>
+            <th></th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  } catch {
+    return <h2>Loading</h2>;
+  }
 }
 
 function SearchBar({ onSetStopNum, onSetStopNumSent }) {
-  const [searchStr, setSearchStr] = useState('5515');
+  const [searchStr, setSearchStr] = useState("5515");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -118,35 +158,44 @@ function SearchBar({ onSetStopNum, onSetStopNumSent }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='input-group'>
-        <input
-          type="text"
-          value={searchStr}
-          onChange={(e) => setSearchStr(e.target.value)}
-          placeholder="Enter Stop number"
-          className='search-textbox form-control' />
-        <span className='input-group-btn'>
-          <button className="btn btn-default search-button" type="submit"> {magnifyingGlass} </button>
-        </span>
+    <div className="header">
+      <div className="container">
+        <br />
+        <h1 className="center title">Metlink Stops</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              value={searchStr}
+              onChange={(e) => setSearchStr(e.target.value)}
+              placeholder="Enter Stop number"
+              className="search-textbox form-control"
+            />
+            <span className="input-group-btn">
+              <button className="btn btn-default search-button" type="submit">
+                {" "}
+                {magnifyingGlass}{" "}
+              </button>
+            </span>
+          </div>
+        </form>
+        <br />
       </div>
-    </form>
+    </div>
   );
 }
 
-
-function HeaderDepartureTable(){
-  const [departures, setDepartures] = useState([]);
-  const [stopNumVal, setStopNum] = useState('5515');
-  const [stopNumSent, setStopNumSent] = useState(false);
+export default function App() {
+  const [departures, setDepartures] = useState(["loading"]);
+  const [stopNumVal, setStopNum] = useState("5515");
+  const [stopNumSent, setStopNumSent] = useState(true);
   // const stopName = "test";
   let stopNumText = stopNumVal;
 
   // TODO: add error handling and auto refresh every 30s
   useEffect(() => {
-    const urlAPI = 'http://localhost:8080/' + stopNumVal; // backend API address
+    const urlAPI = "http://localhost:8080/" + stopNumVal; // backend API address
     if (departures.length && !stopNumSent) {
-
       return;
     }
     const fetchData = async () => {
@@ -160,32 +209,23 @@ function HeaderDepartureTable(){
         console.log("error", error);
       }
     };
-
     fetchData();
   }, [departures, stopNumSent, stopNumVal]);
 
   return (
     <>
-    <div className='header'>
-      <div className='container'>
+      <SearchBar onSetStopNum={setStopNum} onSetStopNumSent={setStopNumSent} />
+
+      <div className="container">
         <br />
-        <h1 className='center title'>Metlink Stops</h1>
-        <SearchBar onSetStopNum={ setStopNum } onSetStopNumSent={ setStopNumSent } />
-        <br />
+        <Suspense fallback={<p>Loading stop departures...</p>}>
+          <h2>
+            {stopNumText.toUpperCase()} &emsp;{" "}
+            {departures.length ? departures[0].stopName : "Fetching stop name"}
+          </h2>
+          <DepartureTable departures={departures} />
+        </Suspense>
       </div>
-    </div>
-    <div className='container'>
-      <br />
-      <h2>{stopNumText.toUpperCase()} &emsp; {departures.length ? departures[0].stopName : "Fetching stop name"}</h2>
-      <DepartureTable departures={departures} />
-    </div>
     </>
   );
 }
-
-export default function App() {  
-  return (
-    <HeaderDepartureTable />
-  );
-}
-
